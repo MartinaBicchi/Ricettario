a9780ab83630a0308a0722b58b26cfbae6904c75
MartinaBicchi <martina.bicchi@stud.unifi.it>
2023-06-06 13:09:24 +0200

    first commit

--- Ricettario/settings.py
+++ Ricettario/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'receipt_website/static')
+    os.path.join(BASE_DIR, 'Ricettario/static')
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
